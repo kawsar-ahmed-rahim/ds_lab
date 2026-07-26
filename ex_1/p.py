6f890993a998793c954bf138984e1626171c6eec
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-07-27 01:29:39 +0600

    first commit

---
+++ ex_1/p.py
@@ -0,0 +1,9 @@
+# Store input numbers
+num1 = 5
+num2 = 10
+
+# Add the numbers
+total = num1 + num2
+
+# Display the sum
+print(f"The sum is: {total}")
---
+++ ex_2/p.py
@@ -0,0 +1,4 @@
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kawsar-ahmed-rahim/ds_lab.git
+git push -u origin main
